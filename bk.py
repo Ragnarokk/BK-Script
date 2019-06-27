5c8ce7a0739e8515ab62511c93db5b5535c6bdb1
Jean-Maurice Martin <jmmartin@centumadeneo.com>
2019-06-27 11:13:28 +0200

    Encapsulation du process dans une fonction

--- bk.py
+++ bk.py
@@ -29,6 +29,130 @@ def next_page(browser: webdriver):
     elem = browser.find_element_by_id('NextButton')  # Find the button
     elem.click()
 
+def completion_process(n: int):
+
+    browser = webdriver.Firefox()
+    
+    for _ in range(args.Niterations):
+        browser.get("https://www.bk-feedback-uk.com")
+    
+        elem = browser.find_element_by_id('NextButton')  # Find the button
+        elem.click()
+    
+        elem = browser.find_element_by_id('SurveyCode')
+        elem.send_keys('23745')
+    
+        #we fill the date
+        time = datetime.datetime.now()
+        time = time - datetime.timedelta(days=7)
+    
+        select = Select(browser.find_element_by_id("InputDay"))
+        select.select_by_value("{:02}".format(time.day))
+    
+        select = Select(browser.find_element_by_id("InputMonth"))
+        select.select_by_value("{:02}".format(time.month))
+    
+        select = Select(browser.find_element_by_id("InputHour"))
+        select.select_by_value('12')
+    
+        select = Select(browser.find_element_by_id("InputMinute"))
+        select.select_by_value('{:02}'.format(time.minute))
+    
+        select = Select(browser.find_element_by_id("InputMeridian"))
+        select.select_by_value("AM")
+    
+        next_page(browser)
+        next_page(browser)
+        next_page(browser)
+    
+        select_radio(browser, 2)
+        next_page(browser)
+    
+        select_radio(browser, 3)
+        next_page(browser)
+    
+        select_radio(browser, 4)
+        next_page(browser)
+    
+        select_radio(browser, 2)
+        next_page(browser)
+    
+        select_radio(browser, 1)
+        next_page(browser)
+    
+        select_column_radio(browser, 1, 5, 5)
+        next_page(browser)
+    
+        select_column_radio(browser, 1, 5, 5)
+        next_page(browser)
+    
+        #select_column_radio(browser, 1, 5, 2)
+        #next_page(browser)
+    
+        select_radio(browser, 1)
+        # il y a des fois deux des fois une seule
+        try:
+            select_radio(browser, 6)
+            print("Two lines")
+        except NoSuchElementException:
+            print("One line")
+        next_page(browser)
+    
+        select_column_radio(browser, 1, 6, 2)
+        next_page(browser)
+    
+        select_radio(browser, 2)
+        next_page(browser)
+    
+        select_column_radio(browser, 1, 5, 2)
+        next_page(browser)
+    
+        next_page(browser)
+    
+        select_checkbox(browser, 18)
+        next_page(browser)
+    
+        select_checkbox(browser, 11)
+        next_page(browser)
+    
+        # we establish a delay because the page takes too long to load
+        # we wait for the radioSimpleInput to appear on the page
+        try:
+            elem = WebDriverWait(browser, 20).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'radioSimpleInput')))
+        except TimeoutException:
+            print("The page takes too long to load")
+    
+        select_radio(browser, 1)
+        next_page(browser)
+    
+        select_radio(browser, 2)
+        next_page(browser)
+    
+        select_radio(browser, 1)
+        next_page(browser)
+    
+        select_radio(browser, 1)
+        next_page(browser)
+    
+        select = Select(browser.find_element_by_id("R069000"))
+        select.select_by_value('9')
+        select = Select(browser.find_element_by_id("R070000"))
+        select.select_by_value('9')
+        select = Select(browser.find_element_by_id("R074000"))
+        select.select_by_value('9')
+        next_page(browser)
+    
+        #select = Select(browser.find_element_by_id("R074000"))
+        #select.select_by_value('9')
+        #next_page(browser)
+    
+        elem = browser.find_element_by_id('S076000')
+        elem.send_keys('69000')
+        next_page(browser)
+    
+        elem = browser.find_element_by_class_name('ValCode')
+        print(elem.text)
+
 # Gestion et parsing des arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-q', '--quit', help='Quit when the program is finished.', action="store_true")
@@ -36,127 +160,6 @@ parser.add_argument('-N', '--Niterations', help='The number of iterations of the
 args = parser.parse_args()
 args.Niterations = args.Niterations if args.Niterations is not None else 1
 
-browser = webdriver.Firefox()
-
-for _ in range(args.Niterations):
-    browser.get("https://www.bk-feedback-uk.com")
-
-    elem = browser.find_element_by_id('NextButton')  # Find the button
-    elem.click()
-
-    elem = browser.find_element_by_id('SurveyCode')
-    elem.send_keys('23745')
-
-    #we fill the date
-    time = datetime.datetime.now()
-    time = time - datetime.timedelta(days=7)
-
-    select = Select(browser.find_element_by_id("InputDay"))
-    select.select_by_value("{:02}".format(time.day))
-
-    select = Select(browser.find_element_by_id("InputMonth"))
-    select.select_by_value("{:02}".format(time.month))
-
-    select = Select(browser.find_element_by_id("InputHour"))
-    select.select_by_value('12')
-
-    select = Select(browser.find_element_by_id("InputMinute"))
-    select.select_by_value('{:02}'.format(time.minute))
-
-    select = Select(browser.find_element_by_id("InputMeridian"))
-    select.select_by_value("AM")
-
-    next_page(browser)
-    next_page(browser)
-    next_page(browser)
-
-    select_radio(browser, 2)
-    next_page(browser)
-
-    select_radio(browser, 3)
-    next_page(browser)
-
-    select_radio(browser, 4)
-    next_page(browser)
-
-    select_radio(browser, 2)
-    next_page(browser)
-
-    select_radio(browser, 1)
-    next_page(browser)
-
-    select_column_radio(browser, 1, 5, 5)
-    next_page(browser)
-
-    select_column_radio(browser, 1, 5, 5)
-    next_page(browser)
-
-    #select_column_radio(browser, 1, 5, 2)
-    #next_page(browser)
-
-    select_radio(browser, 1)
-    # il y a des fois deux des fois une seule
-    try:
-        select_radio(browser, 6)
-        print("Two lines")
-    except NoSuchElementException:
-        print("One line")
-    next_page(browser)
-
-    select_column_radio(browser, 1, 6, 2)
-    next_page(browser)
-
-    select_radio(browser, 2)
-    next_page(browser)
-
-    select_column_radio(browser, 1, 5, 2)
-    next_page(browser)
-
-    next_page(browser)
-
-    select_checkbox(browser, 18)
-    next_page(browser)
-
-    select_checkbox(browser, 11)
-    next_page(browser)
-
-    # we establish a delay because the page takes too long to load
-    # we wait for the radioSimpleInput to appear on the page
-    try:
-        elem = WebDriverWait(browser, 20).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'radioSimpleInput')))
-    except TimeoutException:
-        print("The page takes too long to load")
-
-    select_radio(browser, 1)
-    next_page(browser)
-
-    select_radio(browser, 2)
-    next_page(browser)
-
-    select_radio(browser, 1)
-    next_page(browser)
-
-    select_radio(browser, 1)
-    next_page(browser)
-
-    select = Select(browser.find_element_by_id("R069000"))
-    select.select_by_value('9')
-    select = Select(browser.find_element_by_id("R070000"))
-    select.select_by_value('9')
-    select = Select(browser.find_element_by_id("R074000"))
-    select.select_by_value('9')
-    next_page(browser)
-
-    #select = Select(browser.find_element_by_id("R074000"))
-    #select.select_by_value('9')
-    #next_page(browser)
-
-    elem = browser.find_element_by_id('S076000')
-    elem.send_keys('69000')
-    next_page(browser)
-
-    elem = browser.find_element_by_class_name('ValCode')
-    print(elem.text)
 
 if args.quit:
     browser.quit()
